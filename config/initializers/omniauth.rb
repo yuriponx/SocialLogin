f44371d7a1186b0fd5c7d099b8060ce7b6096dfe
Yuri Hayamizu <yuriponx@gmail.com>
2012-10-18 17:39:17 +0900

    heroku deploy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,"Consumer key","Consumer secret"
-  provider :facebook,"App ID","App Secret" 
+  provider :twitter,"FScpMyIhHMde3qK2eHHGOQ","WL4vsABdEB4A0XjP6Xwu0b2sEV6ewTRXa1rWsrIGXc"
+  provider :facebook,"486939444679559","f6d087538d46339435e35134f83021fa" 
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SocialLogin::Application.config.secret_token = '***'
+SocialLogin::Application.config.secret_token = '06be45da99df4df70fe0e5e39222943c712c2fc62414786fa0cad389af22cb02903f27eb9938fac3e251a806201ff49fc4f4741d7feb3a47e3b73360a7669a4a'
